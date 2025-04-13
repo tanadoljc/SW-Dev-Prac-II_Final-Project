1f31bd220e4c5c3d1a9396615f338eef3ec8fcbf
4Most <mossmost123@gmail.com>
2025-04-13 20:28:41 +0700

    feat: add cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 const auth = require('./routes/auth');
@@ -13,6 +14,13 @@ dotenv.config({path:'./config/config.env'});
 
 connectDB();
 
+const corsOptions = {
+    origin: 'http://localhost:5173', 
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true // Allow cookies/auth headers
+};
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
