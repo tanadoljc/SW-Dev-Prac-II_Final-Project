fd5453d953cd56da0fe6044abfdf35827ebf7121
4Most <mossmost123@gmail.com>
2025-04-13 23:11:49 +0700

    feat: add security

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const auth = require('./routes/auth');
 const massageShops = require('./routes/massage_shops');
 const reservations = require('./routes/reservations');
 
+const mongoSanitize=require('express-mongo-sanitize');
+const helmet=require('helmet');
+const {xss}=require('express-xss-sanitizer');
+const rateLimit=require('express-rate-limit');
+const hpp=require('hpp');
+
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
@@ -23,12 +29,21 @@ const corsOptions = {
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+const limiter=rateLimit({
+    windowsMs:10*60*1000,//10 mins
+    max: 100
+});
+app.use(limiter);
+app.use(hpp())
+
 
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/massageshops', massageShops);
 app.use('/api/v1/reservations', reservations);
 
-// const PORT = process.env.PORT || 5000; // Change for window
 const PORT = process.env.PORT || 5003; // Change for mac
 
 const server = app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, 'mode on port ',PORT));
